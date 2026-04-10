77b263f5519f69019e4aa372dc20669fd236f6d8
amda-phd <amda.prog@gmail.com>
2026-04-10 16:46:39 +0200

    feat(Readings): improve responsivity, heights and scatter display

--- src/pages/Readings/components/ChartsCards/Charts/TempHumChart.tsx
+++ src/pages/Readings/components/ChartsCards/Charts/TempHumChart.tsx
@@ -1,6 +1,15 @@
 import { useTheme } from '@mui/material';
 import type React from 'react';
-import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 import { useChartsContext } from '~ctx/Charts';
 
@@ -12,72 +21,72 @@ export const TempHumChart: React.FC = () => {
 
   return (
     <FallbackChart item="temperature and humidity">
-      <LineChart
-        style={{ height: '30vh', width: '100%' }}
-        responsive
-        data={data}
-        margin={{
-          top: 0,
-          right: -16,
-          left: 0,
-          bottom: 0,
-        }}
-        syncId="anyId"
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="label" orientation="top" ticks={commonTicks} />
-        <YAxis
-          dataKey="temperature"
-          unit="°C"
-          tickCount={4}
-          yAxisId="left"
-          domain={['dataMin-1', 'dataMax+1']}
-        />
-        <YAxis
-          dataKey="humidity"
-          unit="%"
-          tickCount={4}
-          yAxisId="right"
-          orientation="right"
-          domain={['dataMin-1', 'dataMax+1']}
-        />
-        <Tooltip />
-        <Line
-          type="monotone"
-          yAxisId="left"
-          dataKey="temperature"
-          stroke={palette.text.secondary}
-          fill={palette.text.secondary}
-          activeDot={{ r: 8 }}
-          strokeDasharray="5 5"
-        />
-        <Line
-          type="monotone"
-          yAxisId="left"
-          dataKey="temperature_target"
-          stroke={palette.primary.main}
-          dot={false}
-          strokeWidth={2}
-        />
-        <Line
-          type="monotone"
-          yAxisId="right"
-          dataKey="humidity"
-          stroke={palette.warning.main}
-          fill={palette.warning.main}
-          activeDot={{ r: 8 }}
-          strokeDasharray="5 5"
-        />
-        <Line
-          type="monotone"
-          yAxisId="right"
-          dataKey="humidity_target"
-          stroke={palette.success.main}
-          dot={false}
-          strokeWidth={2}
-        />
-        <Legend />
-      </LineChart>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart
+          data={data}
+          margin={{
+            top: 0,
+            right: -16,
+            left: 0,
+            bottom: 0,
+          }}
+          syncId="anyId"
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="label" orientation="top" ticks={commonTicks} />
+          <YAxis
+            dataKey="temperature"
+            unit="°C"
+            tickCount={4}
+            yAxisId="left"
+            domain={['dataMin-1', 'dataMax+1']}
+          />
+          <YAxis
+            dataKey="humidity"
+            unit="%"
+            tickCount={4}
+            yAxisId="right"
+            orientation="right"
+            domain={['dataMin-1', 'dataMax+1']}
+          />
+          <Tooltip />
+          <Line
+            type="monotone"
+            yAxisId="left"
+            dataKey="temperature"
+            stroke={palette.text.secondary}
+            fill={palette.text.secondary}
+            activeDot={{ r: 8 }}
+            strokeDasharray="5 5"
+          />
+          <Line
+            type="monotone"
+            yAxisId="left"
+            dataKey="temperature_target"
+            stroke={palette.primary.main}
+            dot={false}
+            strokeWidth={2}
+          />
+          <Line
+            type="monotone"
+            yAxisId="right"
+            dataKey="humidity"
+            stroke={palette.warning.main}
+            fill={palette.warning.main}
+            activeDot={{ r: 8 }}
+            strokeDasharray="5 5"
+          />
+          <Line
+            type="monotone"
+            yAxisId="right"
+            dataKey="humidity_target"
+            stroke={palette.success.main}
+            dot={false}
+            strokeWidth={2}
+          />
+          <Legend />
+        </LineChart>
+      </ResponsiveContainer>
     </FallbackChart>
   );
 };
