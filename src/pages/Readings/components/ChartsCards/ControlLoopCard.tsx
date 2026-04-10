ff6ac81ab83da85422d82633232b90f4b62468f9
amda-phd <amda.prog@gmail.com>
2026-04-10 16:45:13 +0200

    fix(ControlLoopCard): use the right dataKey

--- src/pages/Readings/components/ChartsCards/ControlLoopCard.tsx
+++ src/pages/Readings/components/ChartsCards/ControlLoopCard.tsx
@@ -10,7 +10,7 @@ export const ControlLoopCard: React.FC = () => (
     <Box display="flex" flexDirection="column" gap={1}>
       <Box display="flex" alignItems="center">
         <Box px={2} ml={1} />
-        <OnOffChart dataKey="humidifier" showXAxis showBrush />
+        <OnOffChart dataKey="control_loop" showXAxis showBrush />
       </Box>
     </Box>
   </InfoCard>
